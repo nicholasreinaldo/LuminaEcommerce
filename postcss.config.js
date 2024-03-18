48cddb27e7c1cce61b2aa600188cc8d0e4003877
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-03-18 10:48:12 +0700

    setup tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -4,13 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  content: ['./src/views/index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./client/public/**/*.{html,js}', './client/public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
