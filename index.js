f5630ed28fb713dabe4c9e4042987735ddd32642
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-05-19 09:07:38 +0000

    May 19, 2024, 4:07 PM

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.get('/api/products', (req, res) => res.json(products))
+
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
-  })
-  
+  console.log(`Server is running at http://localhost:${port}`)
+})
