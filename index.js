43eb81cd8c04aa0e1d5cf805e01c3640f7e8a07b
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-05-29 23:19:26 +0700

    Setting up dynamic product listing using knex and ejs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
 app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, 'client/src/views'))
 
 app.get('/', (req, res) => res.render('index'))
 
