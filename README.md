ed2fe781675364a5e371eebb2fdbef19e6935bcf
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-05-29 23:24:06 +0700

    Project update

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 ﻿# 24001107-44-Nir-Lumina-ChallengeGold
-
-Hi, Welcome to my Binar Academy Gold Challenge github repository.
-This repository will contain all of the related documentations in this project.
-
-Lumina is an skin care products e-commerce website where customer could order their products online, and website admins could change its website content, and process orders.
-
-In summary, this project consists of 4 main pages with its respectives features.
-- Home Page (Main Banner, Product Listing, and About Us)
-- Login & Register Page (Login & Register Account)
-- Admin Page (Modify Products, Update Order Status)
-- Customer Page (See Order Status, Update profile)
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = process.env.PORT || 3000
+
+// Importing all routers from the routes folder
+const indexRouter = require('./server/src/routes/index')
+const adminRouter = require('./server/src/routes/admin')
+const productsRouter = require('./server/src/routes/products')
 
 app.set('view engine', 'ejs')
-
+app.use(express.static(path.join(__dirname, 'client')))
 app.set('views', path.join(__dirname, 'client/src/views'))
 
-app.get('/', (req, res) => res.render('index'))
+app.use('/', indexRouter)
+app.use('/admin', adminRouter)
+app.use('/api', productsRouter)
+
+/* Get Login Page */
+app.get('/login', (req, res) => res.render('login'))
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
