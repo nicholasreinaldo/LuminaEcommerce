78aab1e035c2350f2c25d0ac486ae635ff42ff4f
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-01 05:31:58 +0000

    Jun 1, 2024, 12:31 PM

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const port = process.env.PORT || 3000
 
 // Importing all routers from the routes folder
 const indexRouter = require('./server/src/routes/routes.index')
+const loginRouter = require('/server/src/routes/routes.login')
+const usersRouter = require('./server/src/routes/routes.users')
 const adminRouter = require('./server/src/routes/routes.admin')
 const productsRouter = require('./server/src/routes/routes.products')
-const usersRouter = require('./server/src/routes/routes.users')
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'client')))
@@ -20,13 +21,11 @@ app.use(express.urlencoded({ extended: true }))
 
 /* Using all routers */
 app.use('/', indexRouter)
+app.use('/login', loginRouter)
 app.use('/user', usersRouter)
 app.use('/api', productsRouter)
 app.use('/admin', adminRouter)
 app.use('/api/admin', adminRouter)
-
-/* Get Login Page */
-app.get('/login', (req, res) => res.render('login'))
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
