06468178f3031adf4fcfcf18b20624287806b496
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-01 07:51:25 +0000

    Jun 1, 2024, 2:51 PM

--- client/src/views/js-script/products.js
+++ client/src/views/js-script/products.js
@@ -1,6 +1,10 @@
 // /client/views/js-script/products.js
 
 document.addEventListener('DOMContentLoaded', async () => {
+  function formatPrice(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+  }
+
   try {
     const response = await fetch('/api/products')
     const products = await response.json()
@@ -16,14 +20,16 @@ document.addEventListener('DOMContentLoaded', async () => {
         const imageUrl = `/src/assets/product-images/${product.product_image_url}`
 
         productCard.innerHTML = `
-          <img src="${imageUrl}" alt="${product.product_name}" />
-          <h3>${product.brand_name}</h3>
-          <p>${product.product_name}</p>
-          <div class="product-footer">
-            <span class="price">Rp ${product.product_price}</span>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-        `
+            <img src="${imageUrl}" alt="${product.product_name}" />
+            <h3>${product.brand_name}</h3>
+            <p>${product.product_name}</p>
+            <div class="product-footer">
+              <span class="price">Rp ${formatPrice(
+                product.product_price,
+              )}</span>
+              <button class="add-to-cart">Add to Cart</button>
+            </div>
+          `
 
         productContainer.appendChild(productCard)
       })
