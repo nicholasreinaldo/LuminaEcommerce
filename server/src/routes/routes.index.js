76177cdccf9eddd351ab7f0f20d9c7b242fc1ef4
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-02 05:35:10 +0000

    Jun 2, 2024, 12:35 PM

--- server/src/routes/routes.index.js
+++ server/src/routes/routes.index.js
@@ -3,7 +3,7 @@
 var express = require('express')
 var router = express.Router()
 
-// Route to access index /
+// Route to access index or home page
 router.get('/', (req, res) => res.render('index'))
 
 module.exports = router
