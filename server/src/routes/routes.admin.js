dafd0d750a19c0c4974083df623eeb50fa3b1bda
nicholasr27 <nicholasreinaldo27@gmail.com>
2024-06-02 05:42:20 +0000

    Jun 2, 2024, 12:42 PM

--- server/src/routes/routes.admin.js
+++ server/src/routes/routes.admin.js
@@ -2,10 +2,40 @@
 
 const express = require('express')
 const router = express.Router()
+const knex = require('../../../db')
 const adminController = require('../controllers/controller.admin')
 
-// Route to access /admin
+// Route to access admin dashboard
 router.get('/', (req, res) => res.render('admin'))
+
+// Route to access all products from database for listing in admin dashboard
+router.get('/products', async (req, res) => {
+  try {
+    const products = await knex
+      .select('*')
+      .from('products')
+      .orderBy('id', 'asc') // Order by 'id' or 'created_at'
+    res.json(products)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
+
+// Route to access a single product by ID in the admin dashboard
+router.get('/products/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const product = await knex('products').where({ id }).first()
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' })
+    }
+    res.json(product)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
 
 // Route to create a new product
 router.post('/products', adminController.createProduct)
--- server/src/routes/routes.products.js
+++ server/src/routes/routes.products.js
@@ -3,24 +3,9 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../../../db')
-const adminController = require('../controllers/controller.admin')
 
 // Route to access all products from database for listing in index
 router.get('/products', async (req, res) => {
-  try {
-    const products = await knex
-      .select('*')
-      .from('products')
-      .orderBy('id', 'asc') // Order by 'id' or 'created_at'
-    res.json(products)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ message: 'Internal server error' })
-  }
-})
-
-// Route to access all products from database for listing in admin dashboard
-router.get('/admin/products', async (req, res) => {
   try {
     const products = await knex
       .select('*')
@@ -38,18 +23,6 @@ router.get('/products/:id', async (req, res) => {
   const { id } = req.params
   try {
     const product = await knex('products').where({ id }).first()
-    res.json(product)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ message: 'Internal server error' })
-  }
-})
-
-// Route to access a single product by ID in the admin dashboard
-router.get('/admin/products/:id', async (req, res) => {
-  const { id } = req.params
-  try {
-    const product = await knex('products').where({ id }).first()
     if (!product) {
       return res.status(404).json({ message: 'Product not found' })
     }
@@ -59,7 +32,5 @@ router.get('/admin/products/:id', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' })
   }
 })
-
-router.post('/admin/products', adminController.createProduct)
 
 module.exports = router
